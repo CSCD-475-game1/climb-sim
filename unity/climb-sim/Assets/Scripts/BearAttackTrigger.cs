b0414f28f9f78a95aa54cd0159949d719bf124d6
David Sergio <davidsergio52@gmail.com>
2026-04-27 00:31:18 -0700

    build

--- unity/climb-sim/Assets/Scripts/BearAttackTrigger.cs
+++ unity/climb-sim/Assets/Scripts/BearAttackTrigger.cs
@@ -15,7 +15,7 @@ public class BearAttackTrigger : MonoBehaviour
             PlayerHealth ph = other.GetComponentInChildren<PlayerHealth>(); 
             if (ph != null)
             {
-                ph.TakeDamage(1); 
+                ph.TakeDamage(15); 
             }
         }
     }
